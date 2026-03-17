c7294bacbb5315e0a5381a3a446e1ad1c3f278cc
pleaselouis <pleaselouis@gmail.com>
2026-03-17 16:50:52 +0800

    show object name implement

--- WpfAppYoloDemo/MainWindow.xaml.cs
+++ WpfAppYoloDemo/MainWindow.xaml.cs
@@ -23,6 +23,20 @@ namespace WpfAppYoloDemo
         private int _padX = 0;
         private int _padY = 0;
 
+        private readonly string[] _classNames = new string[]
+{
+    "person", "bicycle", "car", "motorcycle", "airplane", "bus", "train", "truck", "boat",
+    "traffic light", "fire hydrant", "stop sign", "parking meter", "bench", "bird", "cat",
+    "dog", "horse", "sheep", "cow", "elephant", "bear", "zebra", "giraffe", "backpack",
+    "umbrella", "handbag", "tie", "suitcase", "frisbee", "skis", "snowboard", "sports ball",
+    "Kite", "baseball bat", "baseball glove", "skateboard", "surfboard", "tennis racket",
+    "bottle", "wine glass", "cup", "fork", "knife", "spoon", "bowl", "banana", "apple",
+    "sandwich", "orange", "broccoli", "carrot", "hot dog", "pizza", "donut", "cake", "chair",
+    "couch", "potted plant", "bed", "dining table", "toilet", "tv", "laptop", "mouse",
+    "remote", "keyboard", "cell phone", "microwave", "oven", "toaster", "sink", "refrigerator",
+    "book", "clock", "vase", "scissors", "teddy bear", "hair drier", "toothbrush"
+};
+
         public class YoloBox
         {
             public float X { get; set; }
@@ -193,7 +207,13 @@ namespace WpfAppYoloDemo
                 double h = box.H * displayH;
 
                 // 這裡使用 WPF 的 Rect，不需要 OpenCvSharp 前綴
-                host.AddRectangleWithText(new System.Windows.Rect(x, y, w, h), $"ID:{box.ClassId} {box.Confidence:P0}");
+                string label = (box.ClassId >= 0 && box.ClassId < _classNames.Length)
+                   ? _classNames[box.ClassId]
+                   : $"ID:{box.ClassId}";
+
+                string text = $"{label} {box.Confidence:P0}"; // 例如：person 95%
+
+                host.AddRectangleWithText(new System.Windows.Rect(x, y, w, h), text);
             }
         }
     }
